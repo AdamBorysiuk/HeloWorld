b217cc778e38ea105c076b9d1fd29aacd0c7e43b
Adam <Adam@DESKTOP-90MTSKP>
2023-06-06 18:42:18 +0200

    Add project files.

---
+++ HeloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWord
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Naztwan się Adam Borysiuk");
+        }
+    }
+}
+
